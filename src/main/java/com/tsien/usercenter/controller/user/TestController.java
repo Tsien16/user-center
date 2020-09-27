4839540f7cf6828b37f669e2db687b7b8daeac82
qianhoucai <qianhoucai@gerrit.cmss.com>
2020-09-27 14:10:31 +0800

    完成第13章-整合调用链监控Sleuth、Zipkin

--- src/main/java/com/tsien/usercenter/controller/user/TestController.java
+++ src/main/java/com/tsien/usercenter/controller/user/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.xml.crypto.Data;
 import java.util.Date;
 
 /**
